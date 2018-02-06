f1438f92b0d71b3d05044315e54589b2401a1fd8
Paul Anderson <paul.anderson@digital.hmrc.gov.uk>
2018-02-06 11:50:26 +0000

    recursion example of replacing while loop with recursion

--- src/main/scala/com/allaboutscala/Chapter2/fundamentals/whileAndDo/WhileAndDoLoops.scala
+++ src/main/scala/com/allaboutscala/Chapter2/fundamentals/whileAndDo/WhileAndDoLoops.scala
@@ -2,6 +2,11 @@ package com.allaboutscala.Chapter2.fundamentals.whileAndDo
 
 /**
   * Created by paul on 09/01/18.
+  *
+  * Note - within most functional programming languages it is not wise to use 'var' as it enters the realms of mutation
+  * Where possible you should strive to use 'val' which are immutable
+  *
+  * In addition any use of loops should look to use Recursion to mitigate use of vars - See Chapter 10 (recursion) for example
   */
 object WhileAndDoLoops extends App {
 
